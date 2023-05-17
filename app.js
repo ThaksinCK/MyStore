712a17e74307c8b8521b77647d647fb7825574aa
ThaksinCK <thaksin.ckn@gmail.com>
2023-05-17 22:49:00 +0700

    incomplete homepage and database system

--- app.js
+++ app.js
@@ -11,10 +11,54 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/myStore');
 
+const itemSchema = new mongoose.Schema({
+    itemId : String,
+    itemName : String,
+    price : Number,
+    quantity : Number
+});
+const Item = mongoose.model("Items", itemSchema);
+
 const port = 3000;
 
 app.get('/', (req, res)=>{
     res.render('home');
+})
+
+app.get('/home', (req, res)=>{
+    res.render('home');
+})
+
+app.get('/db', (req, res)=>{
+    var itemList = [];
+    Item.find({}).exec()
+    .then(documents => {
+    itemList = documents;
+    res.render('db', {'items' : itemList});
+    })
+    .catch(err => {
+    console.error(err);
+    });
+    })
+
+app.post('/db', (req, res)=>{
+    const itemId = req.body.itemId;
+    const itemName = req.body.itemName;
+    const price = req.body.price;
+    const quantity = req.body.quantity;
+
+    const newItemData = new Item({
+        itemId : itemId,
+        itemName : itemName,
+        price : price,
+        quantity : quantity
+    });
+
+    newItemData.save();
+
+    console.log(`${itemId} ${itemName} ${price} ${quantity}`);
+    
+    res.redirect('/db');
 })
 
 app.listen(port, function(){
